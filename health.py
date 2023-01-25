61859d97e76789dfb65f6da32baea7ca96a2416f
wolethomas78 <wolethomas78@gmail.com>
2023-01-25 20:08:38 +0000

    my third python commit

---
+++ health.py
@@ -0,0 +1,61 @@
+import random
+
+
+
+health = 50
+
+
+
+difficulty = 10
+
+
+
+
+
+potion_health = int(random.randint(25,50) / difficulty)
+
+
+
+
+
+health = health + potion_health
+
+
+
+print(health)
+
+
+
+ky = round(4.7)
+
+print(ky)
+
+
+
+import math
+
+
+
+cook = math.floor(1.5)
+
+print(cook)
+
+
+
+fry = math.ceil(6.2)
+
+print(fry)
+
+
+
+wole = 9 ** 2
+
+print(wole)
+
+
+
+time = math.pow(9,2)
+
+print(time)
+
+
